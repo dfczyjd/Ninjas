57deef6af361dccc78f9d450a5e7a500ecd9b72b
ARTI1208 <31630000+ARTI1208@users.noreply.github.com>
2019-07-14 19:41:11 +0300

    Catch up fix

--- Ninjas/Interpreter/NinjaListener.cs
+++ Ninjas/Interpreter/NinjaListener.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-// Generated from Ninja.g4 by ANTLR 4.7.2
+// Generated from ..\..\..\Interpreter\Ninja.g4 by ANTLR 4.7.2
 
 // Unreachable code detected
 #pragma warning disable 0162
